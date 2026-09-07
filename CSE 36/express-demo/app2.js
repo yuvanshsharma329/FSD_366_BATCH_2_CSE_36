5f4116105dd23a56c5eb3ac44d36d7ba61321796
Yuvansh Sharma <yuvanshsharma534@gamil.com>
2026-09-08 00:44:24 +0530

    FSD

--- CSE 36/express-demo/app2.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-fs.writeFile("Student.txt","Hello student",(err)=>{
-    if(err){
-        console.log("Error creating file:",err);
-        return;
-    }
-    console.log("File created successfully:");
-});

81a5cb5878464a16c7eac77ad6284367f7457301
Yuvansh Sharma <yuvanshsharma534@gamil.com>
2026-09-08 00:56:41 +0530

    FSD

---
+++ Project_2/src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./welcome";
+
+function App() {
+
+return (
+<div>
+    <h1>This Is Main Component</h1> 
+    <Welcome />
+</div>
+);
+}
+
+export default App;
---
+++ Project_2/src/welcome.jsx
@@ -0,0 +1,7 @@
+function Welcome() {
+  return (
+    <h2>Welcome to React Component</h2>
+  );
+}
+
+export default Welcome;
